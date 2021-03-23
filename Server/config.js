6b9c99648adc058016afa5e4cef1c84b1d61a355
superbenji711 <benjilabonte123@gmail.com>
2021-03-23 14:27:03 -0400

    update

--- Server/config.js
+++ Server/config.js
@@ -1,8 +1,9 @@
 
 // oracle.cise.ufl.edu
+const oracleDB_pass = process.env.NODE_ORACLEDB_PASSWORD || "BenjiLabonte7110521";
 
 module.exports = {
-    user          : "hr",
-    password      : process.env.NODE_ORACLEDB_PASSWORD,
-    connectString : "localhost"
+    user          : "blabonte",
+    password      : "BenjiLabonte7110521#",
+    connectString : "oracle.cise.ufl.edu"
   };
