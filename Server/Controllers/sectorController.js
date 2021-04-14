cb6d2739f38ae76bb2cc3af6a90470e0b96965e3
superbenji711 <benjilabonte123@gmail.com>
2021-04-14 11:56:28 -0400

    routing

--- Server/Controllers/sectorController.js
+++ Server/Controllers/sectorController.js
@@ -11,18 +11,7 @@ exports.getAll = async (req, res) => {
     bind={}
     connection = await oracledb.getConnection(config);
     
-<<<<<<< HEAD
     results = await connection.execute(sql);
-=======
-    console.log("Metadata: ");
-     console.dir(result.metaData, { depth: null });
-     console.log("Query results: ");
-     console.dir(result.rows, { depth: null });
-     res.send(result.rows)
-}
-
-exports.get = async (id) => {
->>>>>>> d8a063920331cd9a3ad81fd693061b9258b835af
 
     res.send(results.rows);
      
