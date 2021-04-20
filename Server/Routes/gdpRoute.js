79e33773f1962562369027de773ddf29ff82beca
hutchinsonvandyke <hutchinsonrvandy@ufl.edu>
2021-04-20 09:16:04 -0400

    2 more queries!

--- Server/Routes/gdpRoute.js
+++ Server/Routes/gdpRoute.js
@@ -5,6 +5,9 @@ const express = require('express');
 const gdpRouter = express.Router();
 
 
+gdpRouter.get('/count', gdpController.getCount);
+gdpRouter.get('/:id', gdpController.getGDPStockData); 
+gdpRouter.get('/corr/:id', gdpController.getGDPCorrelation);
 gdpRouter.get('/', gdpController.getAll); //get all
 
 module.exports = gdpRouter;
